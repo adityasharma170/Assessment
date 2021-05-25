128de3e530dd9482cd91b05716e9686f5e194a55
adityasharma170 <84501285+adityasharma170@users.noreply.github.com>
2021-05-25 08:44:48 +0530

    Add files via upload

---
+++ Currency/Program.cs
@@ -0,0 +1,35 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using AssignmentLibrary;
+
+namespace ConsoleApp7
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CurrencyConvertor currencyConvertor = new CurrencyConvertor();
+            int ChoiceConversionType;
+            Console.WriteLine("Choose any method to continue : \n 1-Conversion By using default Conversion Rate  \n 2- Conversion By using own Conversion Rate");
+            ChoiceConversionType = int.Parse(Console.ReadLine());
+            switch (ChoiceConversionType)
+            {
+                case 1:
+                    currencyConvertor.DefaultConversion();
+                    break;
+
+                case 2:
+                    currencyConvertor.UserConversion();
+                    break;
+            }
+            
+           
+
+        }
+    }
+}
