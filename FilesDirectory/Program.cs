a6d807dde950e6d22a824c14643b9c17addb5e96
adityasharma170 <84501285+adityasharma170@users.noreply.github.com>
2021-05-25 08:48:20 +0530

    Add files via upload

---
+++ FilesDirectory/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using AssignmentLibrary;
+namespace ConsoleApp8
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter directory path");
+            string root = @"" + Console.ReadLine();
+           
+            FilesDirectory.DirectorySearch(root);
+            Console.ReadKey();
+        }
+        }
+
+   
+}
